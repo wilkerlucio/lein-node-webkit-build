fb04108b880c2a0d6acc3584752af480ac51704c
Wilker Lucio <wilkerlucio@gmail.com>
2014-08-31 22:59:28 -0300

    correctly extracting nw package and packing app.nw

--- src/leiningen/node_webkit_build.clj
+++ src/leiningen/node_webkit_build.clj
@@ -1,21 +1,45 @@
 (ns leiningen.node-webkit-build
   (:require [node-webkit-build.core :refer :all]
+            [fs.core :as fs]
+            [clojure.string :as str]
             [clojure.java.io :as io]
-            [fs.core :as fs])
-  (:import (java.io File)))
+            [clojure.java.shell :refer [sh with-sh-dir]])
+  (:import (java.io File)
+           (org.apache.commons.io FileUtils)))
+
+(defn path-join [& parts] (str/join (File/separator) parts))
 
 (defn archive-path []
   (let [latest (last (versions-with-data "http://dl.node-webkit.org/"))
         url (get-in latest [:platforms :osx])
         cache-dir "node-webkit-cache"
-        output-path (str cache-dir (File/separator) (fs/base-name url))]
+        output-path (path-join cache-dir (fs/base-name url))]
     (when-not (fs/exists? output-path)
       (io/make-parents output-path)
       (download-with-progress url output-path))
     output-path))
 
+(defn clear-directory [path]
+  (FileUtils/deleteDirectory (io/file path))
+  (fs/mkdirs path))
+
+(defn create-app-archive [source-path target-path]
+  (let [source-path (fs/absolute-path source-path)
+        target-path (fs/absolute-path target-path)]
+    (with-sh-dir source-path
+      (apply sh "zip" "-r" target-path (fs/list-dir source-path))))
+  target-path)
+
+(defn unzip [zip-path target-path]
+  (sh "unzip" "-q" zip-path "-d" target-path))
+
 (defn node-webkit-build
   "Generates a Node-Webkit build."
   [project & args]
-  (let [nw-archive (archive-path)]
-    (println "Extracting from" nw-archive)))
+  (let [release-dir (path-join "releases" "macox")]
+    (clear-directory release-dir)
+    (let [nw-archive (archive-path)]
+      (unzip nw-archive release-dir)
+      (create-app-archive "/Users/wilkerlucio/Development/sm2/smgui/public"
+                          (path-join release-dir "node-webkit-v0.10.2-osx-ia32/node-webkit.app/Contents/Resources" "app.nw"))
+      (println "Extracting from" nw-archive))))
