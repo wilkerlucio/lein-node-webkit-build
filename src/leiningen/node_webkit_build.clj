93c2e79754db1f45965cee54ee816302edcc9097
Wilker Lucio <wilkerlucio@gmail.com>
2014-09-03 10:48:28 -0300

    fixes and using configuration from user project

--- src/leiningen/node_webkit_build.clj
+++ src/leiningen/node_webkit_build.clj
@@ -4,13 +4,7 @@
 
 (defn node-webkit-build
   "Generates a Node-Webkit build."
-  [project & args]
-  (-> (build-app {:root                      "/Users/wilkerlucio/Development/sm2/smgui/public"
-                  :name                      "Subtitle Master"
-                  :platforms                 #{:osx :win :linux32 :linux64}
-                  :osx                       {:icon "/Users/wilkerlucio/Dropbox/Design/subtitle master/subtitle_master.icns"}
-                  :nw-version                "0.10.3"
-                  :disable-developer-toolbar true
-                  :use-lein-project-version  true})
+  [project & _]
+  (-> (build-app (:node-webkit-build project))
       (dissoc :files)
       (pprint)))
