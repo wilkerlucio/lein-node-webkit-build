3f88df6198936fc287d68bea82159002a6208caf
Wilker Lucio <wilkerlucio@gmail.com>
2014-09-04 00:20:53 -0300

    support lein project version and improved error report when configuration is missing

--- src/leiningen/node_webkit_build.clj
+++ src/leiningen/node_webkit_build.clj
@@ -1,8 +1,18 @@
 (ns leiningen.node-webkit-build
   (:require [node-webkit-build.core :refer :all]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [slingshot.slingshot :refer [throw+]]))
 
 (defn node-webkit-build
   "Generates a Node-Webkit build."
   [project & _]
-  (build-app (:node-webkit-build project)))
+  (let [check-nil (fn [req]
+                    (if-not req (throw+ {:type ::nil-configuration
+                                         :message "You need to setup :node-webkit-build on your project.clj"})))
+        use-lein-version (fn [req]
+                           (if (:use-lein-project-version req)
+                             (assoc req :version (:version project))
+                             req))]
+    (build-app (-> (:node-webkit-build project)
+                   check-nil
+                   use-lein-version))))
