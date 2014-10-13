c0ce3077a80dd70a26e26c954e7d802d593e4712
Wilker Lucio <wilkerlucio@gmail.com>
2014-10-12 21:35:13 -0300

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-node-webkit-build "0.1.3-SNAPSHOT"
+(defproject lein-node-webkit-build "0.1.3"
   :description "Builder to pack node-webkit applicatons with leinigen"
   :url "https://github.com/wilkerlucio/lein-node-webkit-build"
   :license {:name "The MIT License"
