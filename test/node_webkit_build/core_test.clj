32918e264c2914ab1086659c201d2d84c772dd7f
Ravindra R. Jaju <jaju@msync.org>
2015-04-04 18:18:37 +0530

    Updated version-checking test (and data) to show latest as 0.12.0

--- test/node_webkit_build/core_test.clj
+++ test/node_webkit_build/core_test.clj
@@ -8,7 +8,9 @@
             [taoensso.timbre :as timbre]))
 
 (def versions-list '("0.8.0" "0.8.1" "0.8.2" "0.8.3" "0.8.4" "0.8.5" "0.8.6" "0.8.7"
-                     "0.9.0" "0.9.1" "0.9.2" "0.9.3" "0.10.0" "0.10.1" "0.10.2"))
+                     "0.9.0" "0.9.1" "0.9.2" "0.9.3" "0.10.0" "0.10.1" "0.10.2"
+                      "0.10.3" "0.10.4" "0.10.5" "0.11.0" "0.11.1" "0.11.2" "0.11.3"
+                      "0.11.4" "0.11.5" "0.11.6" "0.12.0"))
 
 (timbre/set-level! :warn)
 
@@ -25,7 +27,7 @@
   (is (= "0.9.1"
          (:nw-version (normalize-version {:nw-version "0.9.1"
                                           :nw-available-versions versions-list}))))
-  (is (= "0.10.2"
+  (is (= "0.12.0"
          (:nw-version (normalize-version {:nw-version :latest
                                           :nw-available-versions versions-list})))))
 
