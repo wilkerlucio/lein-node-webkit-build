929e23f2a5f4359599ccf5350eb11a3af0e5a514
Wilker Lucio <wilkerlucio@gmail.com>
2016-01-31 14:07:12 -0300

    use snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-node-webkit-build "0.1.8"
+(defproject lein-node-webkit-build "0.1.8-SNAPSHOT"
   :description "Builder to pack node-webkit applicatons with leinigen"
   :url "https://github.com/wilkerlucio/lein-node-webkit-build"
   :license {:name "The MIT License"
